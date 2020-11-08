33ad9864ece5c5b2b0c7e8e46288c6583c23df02
zouuh <zoedur@gmail.com>
2020-11-08 17:48:38 +0100

    💄 UI tests

---
+++ Fall Of Telladena/Assets/Scripts/CanvasController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasController : MonoBehaviour
+{
+    public GameObject canvas;
+
+    public void DisableCanvas() {
+        canvas.SetActive(false);
+    }
+
+    public void ActivateCanvas() {
+        canvas.SetActive(true);
+    }
+}
