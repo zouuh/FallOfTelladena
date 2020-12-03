3f69b175e3d0842e95a0156e80e009f62f7b9a01
zouuh <zoedur@gmail.com>
2020-12-03 17:30:42 +0100

    ✨ Add dialogue reader

---
+++ Fall Of Telladena/Assets/Scripts/ReadTextFile.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class ReadTextFile : MonoBehaviour
+{
+    string[] dialogue;
+    public void Start() {
+        dialogue = ReadString();
+    }
+    static string[] ReadString()
+    {
+        string path = "Assets/Documents/Aïki.txt";
+
+        //Read the text from directly from the test.txt file
+        StreamReader reader = new StreamReader(path);
+        Debug.Log(reader.ReadLine());
+        int nbDialogue = int.Parse(reader.ReadLine());
+        string[] dialogue = new string[nbDialogue];
+        for (int i=0; i<nbDialogue; i++) {
+            string newLine = "";
+            newLine += reader.ReadLine();
+            dialogue[i] = newLine;
+        }
+        reader.Close();
+        return dialogue;
+    }
+
+}
