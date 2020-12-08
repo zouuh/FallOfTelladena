b76ed62df397fb538f462e2015b565aa3db40dea
Manon Sgro <manon-sgro@outlook.fr>
2020-12-08 23:25:43 +0100

    :construction: Water collision

---
+++ Fall Of Telladena/Assets/WaterController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterController : MonoBehaviour
+{
+    public Transform resetPos;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log(other.tag);
+            other.gameObject.transform.position = resetPos.transform.position;
+        }
+    }
+}
