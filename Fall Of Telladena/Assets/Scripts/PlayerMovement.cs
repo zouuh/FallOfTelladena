e6265e076f23a503270e48db7e46c11ab5ce571d
zouuh <zoedur@gmail.com>
2020-12-13 20:07:23 +0100

    :sparkles: Add player animations and moves

--- Fall Of Telladena/Assets/Scripts/PlayerMovement.cs
+++ Fall Of Telladena/Assets/Scripts/PlayerMovement.cs
@@ -7,23 +7,56 @@ using UnityEngine;
 //With a blend tree to control the inputmagnitude and allow blending between animations.
 public class PlayerMovement : MonoBehaviour {
     public CharacterController controller;
-    public Transform camera;
-    public float speed = 6;
+    public Transform cam;
+    public Animator animator;
+    public float speedCoef = 0;
+    public float maxSpeed = 10;
+    public float jumpSpeed = 5;
+    public float gravity = 9.8f;
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
+    bool pickUp = false;
+    float vSpeed = 0;
 
     void Update() {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        pickUp = animator.GetBool("pickUp");
+        if(Input.GetKeyDown("r")) {
+            pickUp = true;
+            animator.SetBool("pickUp", true);
+        }
 
-        if(direction.magnitude >= 0.1f) {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + camera.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+        if(!pickUp) {
+            if(direction.magnitude >= 0.1f) {
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+                if (controller.isGrounded){
+                    vSpeed = 0;
+                }
+
+                // apply gravity acceleration to vertical speed:
+                vSpeed -= gravity * Time.deltaTime;
+
+                Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                moveDir.y = vSpeed;
+                controller.Move(moveDir.normalized * maxSpeed * speedCoef * Time.deltaTime);
+
+                if(speedCoef < 1) {
+                    speedCoef += 0.05f;
+                }
+                
+                animator.SetFloat("speed", speedCoef);
+            }
+            else {
+                if(speedCoef > 0) {
+                    speedCoef -= 0.05f;
+                }
+                animator.SetFloat("speed", speedCoef);
+            }
         }
     }
 }
