7e316f47eceb308996e8418d869bd9ea1f06cb56
zouuh <zoedur@gmail.com>
2020-12-14 15:01:40 +0100

    🔀 CastlePrototype into Prototype

---
+++ Fall Of Telladena/Assets/Scripts/Character.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+using UnityEngine.UI;
+
+public class Character : MonoBehaviour
+{
+    public string[] dialogue;
+    public GameObject dialogueCanvas;
+    public Text dialogueNameText;
+    public Text dialogueText;
+    private static string myName;
+
+    private int dialogueId = 0;
+    private bool isDialoguePossible = false;
+
+
+    void Start() {
+        myName = this.name;
+        dialogue = ReadString();
+    }
+
+    void Update() {
+        if (Input.GetKeyDown("p")) {
+            if (isDialoguePossible) {
+                if (!dialogueCanvas.activeSelf) {
+                    dialogueCanvas.SetActive(true);
+                    dialogueNameText.text = this.name;
+                    dialogueText.text = dialogue[dialogueId];
+                }
+                else {
+                    dialogueCanvas.SetActive(false);
+                }
+            }
+        }
+    }
+
+    public void ChangeDialogueID(int newId) {
+        dialogueId = newId;
+    }
+
+    void OnTriggerStay(Collider colliderInfo) {
+        if (colliderInfo.CompareTag("DialogueInput")) {
+            isDialoguePossible = true;
+        }
+    }
+
+    void OnTriggerExit(Collider colliderInfo) {
+        if (colliderInfo.CompareTag("DialogueInput")) {
+             isDialoguePossible = false;
+        }
+    }
+    static string[] ReadString()
+    {
+        string path = "Assets/Documents/" + myName + ".txt";
+
+        //Read the text from directly from the test.txt file
+        StreamReader reader = new StreamReader(path);
+        Debug.Log(reader.ReadLine());
+        int nbDialogue = int.Parse(reader.ReadLine());
+        string[] dialogue = new string[nbDialogue];
+        for (int i=0; i<nbDialogue; i++) {
+            string newLine = "";
+            newLine += reader.ReadLine();
+            dialogue[i] = newLine;
+        }
+        reader.Close();
+        return dialogue;
+    }
+}
---
+++ Fall Of Telladena/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,63 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This script requires you to have setup your animator with 3 parameters, "InputMagnitude", "InputX", "InputZ"
+//With a blend tree to control the inputmagnitude and allow blending between animations.
+public class PlayerMovement : MonoBehaviour {
+    public CharacterController controller;
+    public Transform cam;
+    public Animator animator;
+    public GameObject dialogueCanvas;
+    public float speedCoef = 0;
+    public float maxSpeed = 10;
+    public float jumpSpeed = 5;
+    public float gravity = 9.8f;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+    bool pickUp = false;
+    float vSpeed = 0;
+
+    void Update() {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        pickUp = animator.GetBool("pickUp");
+        if(Input.GetKeyDown("r")) {
+            pickUp = true;
+            animator.SetBool("pickUp", true);
+        }
+
+        if(!pickUp && !dialogueCanvas.activeSelf) {
+            if(direction.magnitude >= 0.1f) {
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+                if (controller.isGrounded){
+                    vSpeed = 0;
+                }
+
+                // apply gravity acceleration to vertical speed:
+                vSpeed -= gravity * Time.deltaTime;
+
+                Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                moveDir.y = vSpeed;
+                controller.Move(moveDir.normalized * maxSpeed * speedCoef * Time.deltaTime);
+
+                if(speedCoef < 1) {
+                    speedCoef += 0.05f;
+                }
+                
+                animator.SetFloat("speed", speedCoef);
+            }
+            else {
+                if(speedCoef > 0) {
+                    speedCoef -= 0.05f;
+                }
+                animator.SetFloat("speed", speedCoef);
+            }
+        }
+    }
+}
