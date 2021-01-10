044e8fe7647eae84757a93aa72ddcaea8be05a5d
Manon Sgro <manon-sgro@outlook.fr>
2021-01-10 17:34:09 +0100

    :construction: Camera zones and positions in Maze

--- Fall Of Telladena/Assets/Scripts/CamController.cs
+++ Fall Of Telladena/Assets/Scripts/CamController.cs
@@ -1,14 +1,21 @@
-﻿using Cinemachine;
+﻿/*
+ * Authors : (Notslot), Manon
+ */
+
+using Cinemachine;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class CamController : MonoBehaviour
 {
     #region Fields
 
     [SerializeField]
-    private CinemachineVirtualCamera virtualCamera = null;
+    private List<CinemachineVirtualCamera> virtualCameras = null;
     [SerializeField]
     private CinemachineVirtualCamera virtualCameraMain = null;
+    //[SerializeField]
+    //private List<CinemachineVirtualCamera> virtualCamerasMain = null;
 
     #endregion
 
@@ -16,21 +23,41 @@ public class CamController : MonoBehaviour
 
     private void Start()
     {
-        virtualCamera.enabled = false;
+        foreach(CinemachineVirtualCamera cam in virtualCameras)
+        {
+            cam.enabled = false;
+        }
+        //virtualCamera.enabled = false;
     }
 
     private void Update()
     {
         if (Input.GetMouseButton(1))
         {
-            virtualCamera.enabled = true;
+            foreach (CinemachineVirtualCamera cam in virtualCameras)
+            {
+                cam.enabled = true;
+            }
+            //virtualCamera.enabled = true;
         }
         else
         {
+            foreach (CinemachineVirtualCamera cam in virtualCameras)
+            {
+                if (cam.enabled)
+                {
+                    //virtualCameraMain.enabled = false;
+                    virtualCameraMain.transform.eulerAngles = new Vector3(30, virtualCameraMain.m_Follow.rotation.eulerAngles.y, 0);
+                    cam.enabled = false;
+                    //virtualCameraMain.enabled = true;
+                }                
+            }
+            /*
             if (virtualCamera.enabled)
             {
                 virtualCamera.enabled = false;
             }
+            */
         }
     }
 
