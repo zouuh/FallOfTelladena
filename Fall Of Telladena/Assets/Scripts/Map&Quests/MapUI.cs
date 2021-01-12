1fb1730ca9fb6ced1e8ea4d726100f89396a8411
Manon Sgro <manon-sgro@outlook.fr>
2021-01-12 11:13:26 +0100

    :construction: Map movements and markers

--- Fall Of Telladena/Assets/Scripts/Map&Quests/MapUI.cs
+++ Fall Of Telladena/Assets/Scripts/Map&Quests/MapUI.cs
@@ -12,7 +12,11 @@ public class MapUI : MonoBehaviour
 
     [SerializeField]
     private GameObject mapUI; // the entire map UI
+
     private GameObject player; // the player
+
+    [SerializeField]
+    private MapPlayerPosition playerPositionSprite; // the marker for player
 
     #endregion
 
@@ -34,6 +38,9 @@ public class MapUI : MonoBehaviour
             // Desactivate player movements
             player.GetComponent<CharacterController>().enabled = false;
             player.GetComponent<PlayerMovement>().enabled = false;
+
+            // Update player position
+            playerPositionSprite.updatePlayerPosition();
 		}
         else if (Input.GetButtonDown("CloseMap"))
         {
