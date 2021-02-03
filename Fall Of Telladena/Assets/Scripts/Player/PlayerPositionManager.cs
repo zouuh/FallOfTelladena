9d8e196433599d278d3ff7547772b9dd6c4b8b14
Manon Sgro <manon-sgro@outlook.fr>
2021-02-03 16:00:31 +0100

    :bug: Conflict between Scene Loading, DontDestroy and Player Movements

--- Fall Of Telladena/Assets/Scripts/Player/PlayerPositionManager.cs
+++ Fall Of Telladena/Assets/Scripts/Player/PlayerPositionManager.cs
@@ -10,9 +10,9 @@ using UnityEngine.SceneManagement;
 public class PlayerPositionManager : MonoBehaviour
 {
     string previousPlace;
-    private void Awake()
+    /*
+     private void Awake()
     {
-        /*
         Debug.Log("Refresh player!");
         SpawnPoints[] spawnPoints = FindObjectsOfType<SpawnPoints>();
         Debug.Log(spawnPoints.Length);
@@ -25,21 +25,46 @@ public class PlayerPositionManager : MonoBehaviour
                 Debug.Log("Has changed player!");
             }
         }
-        */
     }
+    */
+
     private void Update()
     {
-        Debug.Log("Debug : " + transform.position);
+        //Debug.Log("Debug : " + transform.position);
     }
     public void SetPreviousPlace(string place)
     {
         previousPlace = place;
     }
+    /*
     void OnEnable()
     {
         Debug.Log("OnEnable called");
         SceneManager.sceneLoaded += OnSceneLoaded;
     }
+    */
+    public void SearchNewPosition()
+    {
+        Debug.Log("Refresh player!");
+        SpawnPoints[] spawnPoints = FindObjectsOfType<SpawnPoints>();
+        Debug.Log(spawnPoints.Length);
+        foreach (SpawnPoints spawnPoint in spawnPoints)
+        {
+            if (spawnPoint.isMySpawnPoint(previousPlace))
+            {
+                transform.position = spawnPoint.transform.position;
+                transform.rotation = spawnPoint.transform.rotation;
+                Debug.Log("Has changed player!" + transform.position);
+                /*
+                StartCoroutine(wait(spawnPoint));
+                return;
+                */
+            }
+        }
+        GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>().enabled = true;
+        GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterController>().enabled = true;
+    }
+    /*
     void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
         Debug.Log("Refresh player!");
@@ -47,15 +72,19 @@ public class PlayerPositionManager : MonoBehaviour
         Debug.Log(spawnPoints.Length);
         foreach (SpawnPoints spawnPoint in spawnPoints)
         {
-            /*
             if (spawnPoint.isMySpawnPoint(previousPlace))
             {
-                StartCoroutine(wait(spawnPoint));
-                return;
+                transform.position = spawnPoint.transform.position;
+                transform.rotation = spawnPoint.transform.rotation;
+                Debug.Log("Has changed player!" + transform.position);
+                
+                //StartCoroutine(wait(spawnPoint));
+                //return;
+                
             }
-            */
         }
     }
+*/
 
     IEnumerator wait(SpawnPoints spawnPoint)
     {
@@ -63,5 +92,10 @@ public class PlayerPositionManager : MonoBehaviour
         transform.rotation = spawnPoint.transform.rotation;
         Debug.Log("Has changed player!" + transform.position);
         yield return new WaitForSeconds(.1f);
+    }
+
+    private void Start()
+    {
+        Debug.Log("Current pos : "+transform.position);
     }
 }
