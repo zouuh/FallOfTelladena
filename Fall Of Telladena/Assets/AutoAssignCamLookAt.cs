2cc5763a70b8398f7120068d0a7830b4b9fdc4ea
Manon <manon-sgro@outlook.fr>
2021-03-01 14:28:00 +0100

    :construction: Camera in Forest

---
+++ Fall Of Telladena/Assets/AutoAssignCamLookAt.cs
@@ -0,0 +1,12 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+public class AutoAssignCamLookAt : MonoBehaviour
+{
+    [SerializeField]
+    string tagToLookAt = "Player";
+    void Start()
+    {
+        GetComponent<CinemachineVirtualCamera>().LookAt = GameObject.FindGameObjectWithTag(tagToLookAt).transform;
+    }
+}
