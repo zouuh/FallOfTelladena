9ac3eb8597b890e32df77c5196a74a965d9c7c28
Manon <manon-sgro@outlook.fr>
2021-03-23 13:33:01 +0100

    :lipstick: Improve UI

--- Fall Of Telladena/Assets/Scripts/ActionCanvas.cs
+++ Fall Of Telladena/Assets/Scripts/ActionCanvas.cs
@@ -12,10 +12,16 @@ public class ActionCanvas : MonoBehaviour
     TextMeshProUGUI actionText;
     [SerializeField]
     Image background;
+    /*
     [SerializeField]
     Color baseColor = new Color(0, 0, 0);
     [SerializeField]
     Color disableColor = new Color(100, 100, 100);
+    */
+    [SerializeField]
+    Sprite baseColor;
+    [SerializeField]
+    Sprite disableColor;
 
 
     public void UpdateText(string action, int amount = 1, string tool = "", bool active = true)
@@ -24,12 +30,14 @@ public class ActionCanvas : MonoBehaviour
         if (!active)
         {
             actionText.text += " (nécessite " + (amount != 1 ? amount.ToString() + " " : "") + tool + ")";
-            background.color = disableColor;
+            //background.color = disableColor;
+            background.sprite = disableColor;
             background.GetComponent<RectTransform>().SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 40f, 350f);
         }
         else
         {
-            background.color = baseColor;
+            //background.color = baseColor;
+            background.sprite = baseColor;
             background.GetComponent<RectTransform>().SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 40f, 150f);
         }
     }
